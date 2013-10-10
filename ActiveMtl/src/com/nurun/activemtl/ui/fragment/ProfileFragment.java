a4bb94d3b97b252ec1cff837568dc41b448868f1
Guilhem <gdemiollis@gmail.com>
2013-10-10 15:37:23 -0400

    Clean code

--- ActiveMtl/src/com/nurun/activemtl/ui/fragment/ProfileFragment.java
+++ ActiveMtl/src/com/nurun/activemtl/ui/fragment/ProfileFragment.java
@@ -13,7 +13,6 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.PagerTabStrip;
 import android.support.v4.view.ViewPager;
