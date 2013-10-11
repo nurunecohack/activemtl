46dc1aaad9e3ff82be858b9c5efdf9b1e684d90e
Guilhem <gdemiollis@gmail.com>
2013-10-11 14:37:09 -0400

    Refactor du formulaire

--- ActiveMtl/src/com/nurun/activemtl/ui/fragment/FormFragment.java
+++ ActiveMtl/src/com/nurun/activemtl/ui/fragment/FormFragment.java
@@ -2,9 +2,11 @@ package com.nurun.activemtl.ui.fragment;
 
 import java.io.File;
 import java.lang.ref.WeakReference;
+import java.net.URL;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.location.Location;
 import android.net.Uri;
 import android.os.AsyncTask;
@@ -15,16 +17,22 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.android.gms.location.LocationClient;
 import com.nurun.activemtl.ActiveMtlApplication;
+import com.nurun.activemtl.PreferenceHelper;
 import com.nurun.activemtl.R;
 import com.nurun.activemtl.model.EventType;
 import com.nurun.activemtl.service.UploaderService;
@@ -44,17 +52,16 @@ public class FormFragment extends Fragment {
     private Button suggestionButton;
     private double[] latLong = new double[2];
 
-    private EditText editTextName;
+    private EditText editTextTitle;
     private EditText textDescription;
-    
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        locationClient = (LocationClient) getActivity().getApplicationContext().getSystemService(ActiveMtlApplication.LOCATION_CLIENT);
-    }
+
+    private ImageView imageView;
+
+    private TextView textViewUserName;
 
     public static Fragment newFragment(EventType eventType) {
         FormFragment fragment = new FormFragment();
-        Bundle bundle= new Bundle();
+        Bundle bundle = new Bundle();
         bundle.putSerializable(EXTRA_EVENT_TYPE, eventType);
         fragment.setArguments(bundle);
         return fragment;
@@ -63,13 +70,22 @@ public class FormFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.form_fragment, container, false);
-        suggestionButton = (Button) view.findViewById(R.id.suggestButton);
-        suggestionButton.setEnabled(false);
         suggestionButton.setOnClickListener(onClickListener);
-        editTextName = (EditText) view.findViewById(R.id.editTextName);
-        editTextName.addTextChangedListener(watcher);
-        textDescription = (EditText) view.findViewById(R.id.textDescription);
+        editTextTitle = (EditText) view.findViewById(R.id.editTextTitle);
+        editTextTitle.addTextChangedListener(watcher);
+        textDescription = (EditText) view.findViewById(R.id.editTextDescription);
+        imageView = (ImageView) getView().findViewById(R.id.imageView1);
+        imageView.setOnClickListener(onClickListener);
+        textViewUserName = (TextView) view.findViewById(R.id.textViewUserName);
+        textViewUserName.setText(PreferenceHelper.getUserName(getActivity()));
         return view;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        locationClient = (LocationClient) getActivity().getApplicationContext().getSystemService(ActiveMtlApplication.LOCATION_CLIENT);
+        new ProfilePictureAsyncTask().execute();
     }
 
     @Override
@@ -81,29 +97,41 @@ public class FormFragment extends Fragment {
                 suggestionButton.setEnabled(isSuggestionButtonEnabled(lastLocation));
             }
         }
-        if (!pictureTaken) {
-            pictureTaken = true;
-            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-            File f = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "FindAPlayground");
-            fileUri = Uri.fromFile(f);
-            intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
-            startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
-        }
     }
 
     private boolean isSuggestionButtonEnabled(Location lastLocation) {
-        return lastLocation != null && editTextName.getText().length() > 3;
+        return lastLocation != null && editTextTitle.getText().length() > 3 && pictureTaken;
     }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == FormFragment.CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
             if (resultCode == FragmentActivity.RESULT_OK) {
-                ImageView imageView = (ImageView) getView().findViewById(R.id.imageView1);
                 new BitmapWorkerTask(imageView).execute(fileUri);
             }
         }
         super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.suggestion, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+        case R.id.action_send_suggestion:
+            getActivity().startService(
+                    UploaderService.newIntent(getActivity(), fileUri.getPath(), editTextTitle.getText().toString(), textDescription.getText().toString(),
+                            latLong));
+            NavigationUtil.goToHome(getActivity());
+            break;
+        default:
+            break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     private OnClickListener onClickListener = new OnClickListener() {
@@ -111,9 +139,12 @@ public class FormFragment extends Fragment {
         @Override
         public void onClick(View v) {
             switch (v.getId()) {
-            case R.id.suggestButton:
-                getActivity().startService(UploaderService.newIntent(getActivity(), fileUri.getPath(), editTextName.getText().toString(), textDescription.getText().toString(), latLong));
-                NavigationUtil.goToHome(getActivity());
+            case R.id.imageView1:
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                File f = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "FindAPlayground");
+                fileUri = Uri.fromFile(f);
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+                startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
                 break;
             default:
                 break;
@@ -143,6 +174,7 @@ public class FormFragment extends Fragment {
                 final ImageView imageView = imageViewReference.get();
                 if (imageView != null) {
                     imageView.setImageBitmap(bitmap);
+                    pictureTaken = true;
                 }
             }
         }
@@ -167,4 +199,24 @@ public class FormFragment extends Fragment {
         }
 
     };
+
+    private class ProfilePictureAsyncTask extends AsyncTask<Void, Void, Bitmap> {
+
+        @Override
+        protected Bitmap doInBackground(Void... params) {
+            try {
+                URL profPict = new URL(PreferenceHelper.getProfilePictureUrl(getActivity()));
+                return BitmapFactory.decodeStream(profPict.openStream());
+            } catch (Exception e) {
+                Log.e(getClass().getSimpleName(), e.getMessage(), e);
+                return BitmapFactory.decodeResource(getResources(), R.drawable.ali_g);
+            }
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap result) {
+            super.onPostExecute(result);
+            imageView.setImageBitmap(result);
+        }
+    }
 }
