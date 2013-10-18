a025fdce05be042b87315a8dd3ebcfb1458fbd98
Guilhem <gdemiollis@gmail.com>
2013-10-18 13:08:47 -0400

    Clean code

--- ActiveMtl/src/com/nurun/activemtl/util/NavigationUtil.java
+++ ActiveMtl/src/com/nurun/activemtl/util/NavigationUtil.java
@@ -20,110 +20,87 @@ import com.nurun.activemtl.ui.fragment.ProfileFragment;
 
 public class NavigationUtil {
 
-	public enum NextScreen {
-		Profile, Suggest_Idea, Suggest_Challenge, Suggest_Alert
-	}
+    public enum NextScreen {
+        Profile, Suggest_Idea, Suggest_Challenge, Suggest_Alert
+    }
 
-	public static final int requestLoginCode = 654;
+    public static final int requestLoginCode = 654;
 
-	public static void goToHome(Context context) {
-		context.startActivity(HomeActivity.newIntent(context));
-	}
+    public static void goToHome(Context context) {
+        context.startActivity(HomeActivity.newIntent(context));
+    }
 
-	public static Fragment getFragment(int position, boolean isLoggedIn) {
-		switch (position) {
-		case 0:
-			return HomeFragment.newFragment();
-		case 1:
-			return EventListFragment.newFragment(EventType.Challenge);
-		case 2:
-			return EventListFragment.newFragment(EventType.Alert);
-		case 3:
-			return EventListFragment.newFragment(EventType.Idea);
-		default:
-			throw new IllegalStateException("Not yet implemented");
-		}
-	}
+    public static Fragment getFragment(int position, boolean isLoggedIn) {
+        switch (position) {
+        case 0:
+            return HomeFragment.newFragment();
+        case 1:
+            return EventListFragment.newFragment(EventType.Challenge);
+        case 2:
+            return EventListFragment.newFragment(EventType.Alert);
+        case 3:
+            return EventListFragment.newFragment(EventType.Idea);
+        default:
+            throw new IllegalStateException("Not yet implemented");
+        }
+    }
 
-	public static void goToDetail(Context context, String eventId) {
-		context.startActivity(DetailActivity.newIntent(context, eventId));
-	}
+    public static void goToDetail(Context context, String eventId) {
+        context.startActivity(DetailActivity.newIntent(context, eventId));
+    }
 
-	public static Fragment getNextFragment(NextScreen nextScreen) {
-		switch (nextScreen) {
-		case Profile:
-			return ProfileFragment.newFragment();
-		case Suggest_Alert:
-			return FormFragment.newFragment(EventType.Alert);
-		case Suggest_Challenge:
-			return FormFragment.newFragment(EventType.Challenge);
-		case Suggest_Idea:
-			return FormFragment.newFragment(EventType.Idea);
-		default:
-			throw new IllegalStateException("Wrong click");
-		}
-	}
+    public static Fragment getNextFragment(NextScreen nextScreen) {
+        switch (nextScreen) {
+        case Profile:
+            return ProfileFragment.newFragment();
+        case Suggest_Alert:
+            return FormFragment.newFragment(EventType.Alert);
+        case Suggest_Challenge:
+            return FormFragment.newFragment(EventType.Challenge);
+        case Suggest_Idea:
+            return FormFragment.newFragment(EventType.Idea);
+        default:
+            throw new IllegalStateException("Wrong click");
+        }
+    }
 
-	public static void goToFormFragment(Activity activity,
-			FragmentManager fragmentManager, EventType eventType) {
-		if (PreferenceHelper.isLoggedIn(activity)) {
-			goToFragment(fragmentManager, R.id.suggestion_frame,
-					FormFragment.newFragment(eventType));
-		} else {
-			activity.startActivityForResult(LoginActivity.newIntent(activity),
-					requestLoginCode);
-		}
-	}
+    public static void goToFormFragment(Activity activity, FragmentManager fragmentManager, EventType eventType) {
+        if (PreferenceHelper.isLoggedIn(activity)) {
+            goToFragment(fragmentManager, R.id.suggestion_frame, FormFragment.newFragment(eventType));
+        } else {
+            activity.startActivityForResult(LoginActivity.newIntent(activity), requestLoginCode);
+        }
+    }
 
-	public static void goToProfile(Activity activity,
-			FragmentManager fragmentManager) {
-		if (PreferenceHelper.isLoggedIn(activity)) {
-			goToFragment(fragmentManager, R.id.content_frame,
-					ProfileFragment.newFragment());
-		} else {
-			activity.startActivityForResult(LoginActivity.newIntent(activity),
-					requestLoginCode);
-		}
-	}
+    public static void goToProfile(Activity activity, FragmentManager fragmentManager) {
+        if (PreferenceHelper.isLoggedIn(activity)) {
+            goToFragment(fragmentManager, R.id.content_frame, ProfileFragment.newFragment());
+        } else {
+            activity.startActivityForResult(LoginActivity.newIntent(activity), requestLoginCode);
+        }
+    }
 
-	private static NextScreen getNextScreenFromEventType(EventType eventType) {
-		switch (eventType) {
-		case Idea:
-			return NextScreen.Suggest_Idea;
-		case Challenge:
-			return NextScreen.Suggest_Challenge;
-		case Alert:
-			return NextScreen.Suggest_Alert;
-		default:
-			throw new IllegalStateException("Wrong click");
-		}
-	}
+    public static void handleMenuClick(FragmentActivity activity, int position) {
+        FragmentManager fragmentManager = activity.getSupportFragmentManager();
+        if (position == 4) {
+            NavigationUtil.goToProfile(activity, fragmentManager);
+        } else {
+            Fragment fragment = getFragment(position, PreferenceHelper.isLoggedIn(activity));
+            goToFragment(fragmentManager, R.id.content_frame, fragment);
+        }
+    }
 
-	public static void handleMenuClick(FragmentActivity activity, int position) {
-		FragmentManager fragmentManager = activity.getSupportFragmentManager();
-		if (position == 4) {
-			NavigationUtil.goToProfile(activity, fragmentManager);
-		} else {
-			Fragment fragment = getFragment(position,
-					PreferenceHelper.isLoggedIn(activity));
-			goToFragment(fragmentManager, R.id.content_frame, fragment);
-		}
-	}
-
-	private static void goToFragment(FragmentManager fragmentManager,
-			int frameId, Fragment fragment) {
-		FragmentTransaction transaction = fragmentManager.beginTransaction();
-		Fragment mapFragment = fragmentManager
-				.findFragmentById(R.id.map_fragment);
-		if (mapFragment != null) {
-			transaction.remove(mapFragment);
-		}
-		Fragment listFragment = fragmentManager
-				.findFragmentById(R.id.list_fragment);
-		if (listFragment != null) {
-			transaction.remove(listFragment);
-		}
-		transaction.replace(frameId, fragment).commit();
-	}
+    private static void goToFragment(FragmentManager fragmentManager, int frameId, Fragment fragment) {
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        Fragment mapFragment = fragmentManager.findFragmentById(R.id.map_fragment);
+        if (mapFragment != null) {
+            transaction.remove(mapFragment);
+        }
+        Fragment listFragment = fragmentManager.findFragmentById(R.id.list_fragment);
+        if (listFragment != null) {
+            transaction.remove(listFragment);
+        }
+        transaction.replace(frameId, fragment).commit();
+    }
 
 }
